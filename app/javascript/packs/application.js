78bcb390dc14c7a07065fb4a689528318c028baf
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-01-22 20:55:34 -0300

    build(main/styles): Config Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,24 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import * as bootstrap from "bootstrap"
+window.bootstrap
+
+import jquery from "jquery"
+window.jQuery = jquery
+window.$ = jquery
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
 
 Rails.start()
 ActiveStorage.start()
