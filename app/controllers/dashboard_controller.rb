074abba2dea809230bd6d6dc78126108e3b70230
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-01-23 13:54:05 -0300

    feat(main/dash): Add Expenses/Income due today

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,5 +5,10 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   before_action :set_resources, only: %i[index]
   layout 'dashboard'
-  def index; end
+  def index
+    @expire_today = Installment.where(status: 'unpaid', due_date: Date.today).order(due_date: :asc)
+    @expire_week = Installment.where(status: 'unpaid')
+                              .where('due_date > ? and due_date <= ?', Date.today, Date.today + 1.week)
+                              .order(due_date: :asc)
+  end
 end
