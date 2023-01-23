492af905cea69815766b50c0158b75f2c7a76085
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-01-23 15:10:49 -0300

    feat(main): Add whenever for execute rake create banks

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'devise'
 gem 'cocoon'
 gem 'draper'
 gem 'httparty'
+gem 'whenever', require: false
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
---
+++ config/schedule.rb
@@ -0,0 +1,24 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+every 1.month, at: 'start of the month at 2am' do
+  rake "create_banks: create"
+end
+
+# Learn more: http://github.com/javan/whenever
