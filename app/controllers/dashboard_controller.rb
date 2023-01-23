fea9c21fd35aa34bee00c91cbe395b011f82f71d
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-01-22 21:51:15 -0300

    feat(main/users): Adjusted email confirmation for new users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
   layout 'dashboard'
   def index; end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def icon(icon, options = {})
+    file = File.read("node_modules/bootstrap-icons/icons/#{icon}.svg")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    svg['class'] += " #{options[:class]}" if options[:class].present?
+    svg['width'] = options[:width] if options[:width].present?
+    svg['height'] = options[:height] if options[:height].present?
+    svg['viewBox'] = options[:viewBox] if options[:viewBox].present?
+    doc.to_html.html_safe
+  end
 end
