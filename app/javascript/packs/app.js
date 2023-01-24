ae27016418909f27c29b1b7dfa1b4224bd52a254
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-01-23 23:37:12 -0300

    feat(main/views): Add counter in installments

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -58,26 +58,3 @@ sidePanelDrop.addEventListener('click', (e) => {
 
 
 
-/* ====== Mobile search ======= */
-const searchMobileTrigger = document.querySelector('.search-mobile-trigger');
-const searchBox = document.querySelector('.app-search-box');
-
-searchMobileTrigger.addEventListener('click', () => {
-
-	searchBox.classList.toggle('is-visible');
-	
-	let searchMobileTriggerIcon = document.querySelector('.search-mobile-trigger-icon');
-	
-	if(searchMobileTriggerIcon.classList.contains('fa-search')) {
-		searchMobileTriggerIcon.classList.remove('fa-search');
-		searchMobileTriggerIcon.classList.add('fa-times');
-	} else {
-		searchMobileTriggerIcon.classList.remove('fa-times');
-		searchMobileTriggerIcon.classList.add('fa-search');
-	}
-	
-		
-	
-});
-
-
